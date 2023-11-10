646fdb885282ae84970156cce50a3e8f82d4a556
Vladislav Zhogol <vzhogol176@gmail.com>
2023-11-10 22:08:06 +0300

    Create tcp server class

---
+++ slae_solver/Server/TcpServer.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Sockets;
+
+namespace Server
+{
+    public class TcpServer : IDisposable
+    {
+        private readonly TcpListener _tcpListener;
+        private bool _isDisposed = false;
+        private Settings _settings;
+        public TcpServer()
+        {
+            _settings = new Settings();
+            _tcpListener = new TcpListener(_settings.IP, _settings.Port);
+            SetUpThreadPool();
+        }
+
+        public void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                _tcpListener.Stop();
+                _isDisposed = true;
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        private void SetUpThreadPool()
+        {
+            ThreadPool.SetMinThreads(_settings.MinThreadCount, _settings.MinThreadCount);
+            ThreadPool.SetMaxThreads(_settings.MaxThreadCount, _settings.MaxThreadCount);
+        }
+    }
+}
